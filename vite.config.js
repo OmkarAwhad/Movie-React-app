334d50a371c87d47a8773bc37b141a5348519e45
omkar awhad <omkarawhad3007@gmail.com>
2024-07-01 19:35:48 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Movie-React-app",
   plugins: [react()],
+  base: "/Movie-React-app",
 })
